720f351e3a19b34b6aeb164ae952263f460f9aae
kfh <kfh@kfh.no>
2015-02-13 19:48:31 +0100

    this file should be a part of the previous changeset.

---
+++ spaces-search-api-server/src/spaces_search_api_server/service/sync.clj
@@ -0,0 +1,22 @@
+(ns spaces-search-api-server.service.sync
+  (:require [taoensso.timbre :as timbre]
+            [com.stuartsierra.component :as component]  
+            [clojure.core.async :refer [chan close!]]))
+
+(timbre/refer-timbre)
+
+(defrecord LocationSync []
+  component/Lifecycle
+  
+  (start [this]
+    (info "Starting location sync")
+    (if (:sync this)
+      this))
+  
+  (stop [this]
+    (info "Stopping location sync")
+    (if-not (:sync this)
+      this)))
+
+(defn location-sync []
+  (map->LocationSync {}))
